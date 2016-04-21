f4ea7704a4c51e7b549ac41968107d82a585a6e2
Richard Stewing <richy.sting@gmail.com>
2016-04-21 05:29:01 -0700

    changes

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,45 @@
+//use std::thread;
+
+struct Point {
+  x : i32,
+  y : i32,
+}
+
+impl Point {
+    fn new(x : i32, y: i32) -> Point{
+        Point {
+            x: x,
+            y: y,
+        }
+    }
+    
+    fn print(&self){
+        println!("({}, {})", self.x, self.y);
+    }
+    
+    fn add(&self, p: Point) -> Point{
+        Point {
+            y:self.y + p.y,
+            x:self.x + p.x,
+        }
+    }
+    
+        
+    
+}
+
+
+
 fn main() {
+    let p = Point::new(12, 21);
+    let p2 = Point::new(21, 12);
+    let h = thread::spawn(p.print());
+    //thread::sleep_ms(100);
     println!("Hello, world!");
+    p.print();
+    let sum = p.add(p2);
+    sum.print();
+    //h.join().unwrap();
+
+    
 }
