2973d18eb4ba18dc4d28e9b7c2c826021c36a99b
Richard Stewing <richy.sting@gmail.com>
2017-11-19 15:03:52 +0100

    natural number algebra example

---
+++ projects/hello_world/natural_num/target/debug/naturalNum.d
@@ -0,0 +1,1 @@
+/usr/home/haetze/Documents/Code/rust/projects/hello_world/natural_num/target/debug/naturalNum: /usr/home/haetze/Documents/Code/rust/projects/hello_world/natural_num/src/main.rs
---
+++ projects/hello_world/natural_num/target/debug/natural_num.d
@@ -0,0 +1,1 @@
+/usr/home/haetze/Documents/Code/rust/projects/hello_world/natural_num/target/debug/natural_num: /usr/home/haetze/Documents/Code/rust/projects/hello_world/natural_num/src/main.rs
