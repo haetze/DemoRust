e897cdca43bca7daa3a121fd3d3bc21c2554a6fb
haetze <richy.sting@gmail.com>
2017-11-29 08:57:16 +0100

    new, add, remove_head Funktionen

--- projects/hello_world/mutable_linked_list/src/main.rs
+++ projects/hello_world/mutable_linked_list/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
 use std::fmt;
 use std::mem;
@@ -11,7 +13,7 @@ impl<A: fmt::Display> fmt::Display for List<A>{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self{
             List::Nil => write!(f, "Nil"),
-            List::Cons(ref h, ref t) => write!(f, "({} {}\n)", h, t)
+            List::Cons(ref h, ref t) => write!(f, "({} {})", h, t)
         }        
     }
 }
@@ -24,11 +26,11 @@ trait MutableMap<A>{
     fn mutable_map(&mut self, fn(&A) -> A);
 }
 
-fn list_create(i:i32) -> List<i32>{
-    if i == 0{
+fn list_create(i:f32) -> List<f32>{
+    if i <= 0.0{
         List::Nil
     }else{
-        List::Cons(Box::new(i), Box::new(list_create(i-1)))
+        List::Cons(Box::new(i), Box::new(list_create(i-1.0)))
     }
 }
 
@@ -53,12 +55,42 @@ impl<A,B> ImmutableMap<A,B> for List<A>{
         }
     }
 }
+
+impl<A> List<A>{
+    fn new() -> List<A>{
+        List::Nil
+    }
+
+    fn add(self, a:A) -> List<A>{
+        List::Cons(Box::new(a), Box::new(self))
+    }
+    fn remove_head(self) -> List<A>{
+        match self {
+            List::Nil => panic!("Empty List"),
+            List::Cons(h, t) => *t,
+        }
+    }
+    
+}
             
 
 fn main() {
-    let mut l = list_create(10);
-    l.mutable_map(|x| 2*x);
-    let l2 = l.immutable_map(|x| list_create(*x));
-    //println!("{}", l);
-    println!("{}", l2);
+    let l1 : List<f32> = List::new();
+    let l2 = l1.add(2.0);
+    let mut l3 = l2.add(4.0);
+    l3.mutable_map(|x| x.sqrt());
+    println!("{}", l3);
+    //let empty : List<i32> = List::new();
+    //empty.remove_head();
+    let l4 = l3.remove_head();
+    println!("{}", l4);
+    //println!("{}", l2);
+    /*let mut a = 2;
+    loop{
+        a = a + a;
+
+        println!("{}",a);
+            
+
+    }*/
 }
