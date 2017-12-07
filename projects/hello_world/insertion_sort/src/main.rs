44f2b545b78a5f5c839a0d4fcd1c33d9da7b087e
haetze <richy.sting@gmail.com>
2017-12-07 08:14:13 +0100

    sorting

---
+++ projects/hello_world/insertion_sort/src/main.rs
@@ -0,0 +1,180 @@
+extern crate colored;
+extern crate rand;
+
+use rand::Rng;
+use colored::*;
+
+trait Sort<A:PartialOrd> {
+    fn insertion_sort_mut(&mut self);
+    fn insertion_sort(&self) -> Vec<&A>;
+    fn quick_sort_mut(&mut self);
+    fn quick_sort(&self) -> Vec<&A>;
+    fn selection_sort_mut(&mut self);
+    fn selection_sort(&self) -> Vec<&A>;
+    fn is_sorted(&self) -> bool;
+    fn check_sorted_output(&self);
+}
+
+fn random_vec(i: i32) -> Vec<i32>{
+    let mut rng = rand::thread_rng();
+    let mut v = Vec::new();
+    for _ in 0..i{
+        v.push(rng.gen());
+    }
+    return v;
+}
+
+
+
+fn main() {
+    let i = 3;
+    /*println!("\n\n\n+++++++++++++++\t Insertion Sort\t+++++++++++++++\n");
+    {
+        let mut v1 = random_vec(i);
+        v1.reverse();
+        println!("Original Vector: {:?}\n", v1);
+        {
+            let v2 = v1.insertion_sort();
+            println!("Sortet Vector(copy, insertion sort):{:?}\n", v2);
+            v2.check_sorted_output();
+            println!("Original Vector:{:?}\n", v1);
+        }
+        v1.insertion_sort_mut();
+        println!("Original Vector (sorted, insertion sort):{:?}\n", v1);
+        v1.check_sorted_output();
+    }*/
+    println!("\n\n\n+++++++++++++++\t Quick Sort\t+++++++++++++++\n");
+    {
+        let mut v1 = random_vec(i);
+        v1.reverse();
+        println!("Original Vector: {:?}\n", v1);
+        {
+            let v2 = v1.quick_sort();
+            println!("Sortet Vector(copy, quick sort):{:?}\n", v2);
+            v2.check_sorted_output();
+            println!("Original Vector:{:?}\n", v1);
+        }
+        v1.quick_sort_mut();
+        println!("Original Vector (sorted, quick sort):{:?}\n", v1);
+        v1.check_sorted_output();
+    }
+    /*println!("\n\n\n+++++++++++++++\t Selection Sort\t+++++++++++++++\n");
+    {
+        let mut v1 : Vec<i32>= random_vec(i);
+        v1.reverse();
+        println!("Original Vector: {:?}\n", v1);
+        {
+            let v2 = v1.selection_sort();
+            println!("Sortet Vector(copy, selection sort):{:?}\n", v2);
+            v2.check_sorted_output();
+            println!("Original Vector:{:?}\n", v1);
+        }
+        v1.selection_sort_mut();
+        println!("Original Vector (sorted, selection sort):{:?}\n", v1);
+        v1.check_sorted_output();
+    }*/
+    
+}
+
+
+
+impl<A:PartialOrd> Sort<A> for [A]{
+
+    fn is_sorted(&self) -> bool{
+        let mut pre = 0;
+        for i in 1..self.len(){
+            if self[i] < self[pre]{
+                return false;
+            }
+            pre = pre + 1;
+        }
+        return true;
+    }
+
+    fn check_sorted_output(&self){
+        if !self.is_sorted(){
+            println!("{}\n", "Not sorted!".red().bold());
+        }else{
+            println!("{}\n", "Sorted!".green().bold());
+        }
+    }
+
+    fn selection_sort_mut(&mut self){
+        let n = self.len();
+        let mut links = 0;
+        while links < n {
+            let mut min = links;
+            for i in (links+1)..n {
+                if self[i] < self[min] {
+                    min = i; 
+                }
+            }
+            self.swap(min, links);
+            links = links + 1;
+        }
+    }
+
+    fn selection_sort(&self) -> Vec<&A>{
+        let mut v = Vec::new();
+        for n in self {
+            v.push(n);
+        }
+        v.selection_sort_mut();
+        return v;
+    }
+
+    
+    fn quick_sort_mut(&mut self){
+        if 1 < self.len() {
+            let (mut pivot, mut high) = (0, self.len()-1);
+            for _ in 0..self.len()-1 {
+                if self[pivot] > self[high]{
+                    self.swap(pivot, high);
+                    high = high - 1;
+                }else if self[pivot] <= self[high]{
+                    self.swap(pivot + 1, high);
+                    pivot = pivot + 1;
+                }                
+            }
+            self[..pivot].quick_sort();
+            self[pivot+1..].quick_sort();
+        }
+       
+    }
+
+    fn quick_sort(&self) -> Vec<&A>{
+        let mut v = Vec::new();
+        for n in self{
+            v.push(n);
+        }
+        v.insertion_sort_mut();
+        return v;
+    }
+
+  
+        
+    
+    fn  insertion_sort_mut(&mut self){
+        let mut i = 1;
+        while i < self.len() {
+            let mut j = i;
+            while j > 0 && self[j-1] > self[j] {
+                self.swap(j-1, j);
+                j = j - 1;
+            }
+            i = i + 1;
+        }
+    }
+    
+    fn  insertion_sort(&self) -> Vec<&A>{
+        let mut v = Vec::new();
+        for n in self{
+            v.push(n);
+        }
+        v.insertion_sort_mut();
+        return v;
+
+    }
+}
+
+    
---
+++ projects/hello_world/insertion_sort/target/debug/insertion_sort.d
@@ -0,0 +1,1 @@
+/Users/haetze/Documents/Code/rust/DemoRust/projects/hello_world/insertion_sort/target/debug/insertion_sort: /Users/haetze/Documents/Code/rust/DemoRust/projects/hello_world/insertion_sort/src/main.rs
