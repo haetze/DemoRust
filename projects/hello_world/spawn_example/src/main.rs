8ac964cbe8fd7f69338d8cc7de4dd6272673a1b7
Richard Stewing <richy.sting@gmail.com>
2018-01-27 21:03:11 +0100

    io added to spawn example

--- projects/hello_world/spawn_example/src/main.rs
+++ projects/hello_world/spawn_example/src/main.rs
@@ -1,7 +1,20 @@
+#![allow(unused_must_use)]
+
+
 use std::thread;
+use std::io::{self};
 
 fn main() {
-    let n = 10;
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer);
+    let n = match buffer.trim().parse::<i32>() {
+        Ok(i)   => i,
+        Err(..) => {
+            println!("No Number was typed in, use default 10");
+            10
+        },
+    };
+    
     let mut handler_vec = Vec::new();
     for i in 0..n{
         handler_vec.push(thread::spawn(move || {
