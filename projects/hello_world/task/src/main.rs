ac5fb8a6728ab089fccb0108f603d9aed58e7af5
Richard Stewing <richy.sting@gmail.com>
2018-03-11 20:56:00 +0100

    joining

--- projects/hello_world/task/src/main.rs
+++ projects/hello_world/task/src/main.rs
@@ -1,39 +1,40 @@
 #![feature(proc_macro, conservative_impl_trait, generators)]
 
 
+
 extern crate futures_await as futures;
 extern crate futures_cpupool;
 
+
 use futures::prelude::*;
-use futures::future::*;
 use futures_cpupool::CpuPool;
 use futures::sync::mpsc::*;
 use std::thread;
         
 
+
+
 #[async]
-fn create_future(x :i32) -> Result<i32, String> {
+fn create_future(x :i32) -> Result<i32, ((), Receiver<i32>)> {
     Ok(x)
 }
 
 
 fn main() {
-    let pool = CpuPool::new(1);
-    let a : i32 = 1;
+    let pool = CpuPool::new(8);
+    let a : i32 = 10;
     let mut future_vec = Vec::new();
-    let mut future_vec2 = Vec::new();
     let mut channel_vec = Vec::new();
     for i in 0..a{
         let (mut tx, mut rx) = channel::<i32>(1);
         let channel_future = rx.into_future();
-        let result = channel_future.and_then(|result| {
-            let y : FutureResult<i32, i32> = match result {
-                (Some(n), _) => ok(n),
-                (_,_)        => err(0),
+        let result = channel_future.and_then(move |(r,z)| {
+            println!("In Channel future: {}", i);
+            let y = match r {
+                Some(n) => Ok(n),
+                _       => Err(((), z)),
             };
-            //let m :Result<i32, i32>= Err(3);
-            //println!("{:?}", m);
-            Ok(y)
+            y
         });
         channel_vec.push(tx);
         let f = create_future(i).and_then(move |x| {
@@ -52,9 +53,9 @@ fn main() {
             println!("Future {} running", i);
             Ok(x)
         });
-        
-        future_vec.push(pool.spawn(f));
-        future_vec2.push(pool.spawn(result));
+
+        let t = result.join(f).and_then(|(a,b)| Ok(a+b));
+        future_vec.push(pool.spawn(t));
     }
 
     channel_vec.reverse();
@@ -65,12 +66,8 @@ fn main() {
         b = b - 1;
     }
     
-    /*for v in future_vec {
+    for v in future_vec {
         println!("Result of a Future: {:?}", v.wait());
-    }*/
-
-    for v in future_vec2 {
-        println!("Channel future result: {:?}", v.wait());
     }
 
     
