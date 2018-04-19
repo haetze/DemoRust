3c91ba0cb498355f7d9da60081e17ebf9cb499fc
haetze <richy.sting@gmail.com>
2018-04-19 09:27:44 +0200

    generic

--- projects/biparit/src/main.rs
+++ projects/biparit/src/main.rs
@@ -4,7 +4,7 @@
 fn main() {
 
     let nodes :Vec<i32>       = (0..6).collect();
-    let edges :Vec<(i32,i32)> = vec![(0,1),(0,3),(1,3),(1,4),(1,5),(2,4)];
+    let edges :Vec<(i32,i32)> = vec![(0,3),(1,3),(1,4),(1,5),(2,4)];
 
     let (w,v) = find_biparit(&nodes, &edges).unwrap();    
         
@@ -13,16 +13,16 @@ fn main() {
 }
 
 
-fn find_biparit(nodes: &[i32], edges: &[(i32,i32)]) -> Option<(Vec<i32>, Vec<i32>)>{
+fn find_biparit<A: Eq + Copy>(nodes: &[A], edges: &[(A,A)]) -> Option<(Vec<A>, Vec<A>)>{
 
-    let mut w :Vec<i32> = Vec::new();
-    let mut v :Vec<i32> = Vec::new();
+    let mut w :Vec<A> = Vec::new();
+    let mut v :Vec<A> = Vec::new();
     
     for &node in nodes {
-        if find(node, &w) && find(node, &v) {
+        if find(&node, &w) && find(&node, &v) {
             println!("Konnte keine Teilung finden");
-            return None::<(Vec<i32>, Vec<i32>)>;
-        }else if find(node, &w) {
+            return None::<(Vec<A>, Vec<A>)>;
+        }else if find(&node, &w) {
             for &(a, b) in edges {
                 if node == a {
                     push_once(&mut v,b);
@@ -30,7 +30,7 @@ fn find_biparit(nodes: &[i32], edges: &[(i32,i32)]) -> Option<(Vec<i32>, Vec<i32
                     push_once(&mut v,a);
                 }
             }
-        }else if find(node, &v) {
+        }else if find(&node, &v) {
             for &(a, b) in edges{
                 if node == a {
                     push_once(&mut w,b);
@@ -42,19 +42,19 @@ fn find_biparit(nodes: &[i32], edges: &[(i32,i32)]) -> Option<(Vec<i32>, Vec<i32
         }else{
             let mut choice = 0;
             for &(a, b) in edges{
-                if node == a && find(b, &w) {
+                if node == a && find(&b, &w) {
                     choice = 2;
                     break;
                 }
-                if node == a && find(b, &v) {
+                if node == a && find(&b, &v) {
                     choice = 1;
                     break;
                 }
-                if node == b && find(a, &w) {
+                if node == b && find(&a, &w) {
                     choice = 2;
                     break;
                 }
-                if node == b && find(a, &v) {
+                if node == b && find(&a, &v) {
                     choice = 1;
                     break;
                 }
@@ -87,16 +87,16 @@ fn find_biparit(nodes: &[i32], edges: &[(i32,i32)]) -> Option<(Vec<i32>, Vec<i32
 
 }
 
-fn push_once(vec: &mut Vec<i32>, i: i32){
-    if ! find(i, &vec){
+fn push_once<A:Eq + Copy>(vec: &mut Vec<A>, i: A){
+    if ! find(&i, &vec){
         vec.push(i);
     }
 }
 
 
-fn find(i: i32, vec: &Vec<i32>) -> bool {
+fn find<A: Eq>(i: &A, vec: &[A]) -> bool {
     for n in vec {
-        if *n == i {
+        if n == i {
             return true;
         }
     }
