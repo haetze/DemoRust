720c7826d6ce7c8b678d54e1557fa1ffb1a04b76
Richard Stewing <richy.sting@gmail.com>
2018-05-26 16:41:46 +0200

    mem::swap

--- projects/mem_test/src/main.rs
+++ projects/mem_test/src/main.rs
@@ -3,6 +3,26 @@ struct Container<A> {
     c: A,
 }
 
+#[derive(Debug)]
+struct List<A> {
+    l: Box<InnerList<A>>,
+}
+
+#[derive(Debug)]
+enum InnerList<A> {
+    Nil,
+    Cons(A, Box<InnerList<A>>),
+}
+
+impl<A> List<A> {
+    fn insert(&mut self, a:A){
+        use std::mem::swap;
+        let mut l = Box::new(InnerList::Nil);
+        swap(&mut l, &mut self.l);
+        self.l = Box::new(InnerList::Cons(a, l));
+    }
+}
+
 impl<A> Container<A> {
     fn change(&mut self, x:A){
         self.c = x;
@@ -20,4 +40,10 @@ fn main() {
     println!("{:?}", c);
     c.use_fn(|x| x*2);
     println!("{:?}", c);
+    let mut l: List<i32> = List{ l: Box::new(InnerList::Nil)};
+    println!("{:?}", l);
+    for i in 0..10 {
+        l.insert(i);
+        println!("{:?}", l);
+    }
 }
