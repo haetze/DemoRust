77903a627d9b0e0e337ad42b1067231520313a6b
Richard Stewing <richy.sting@gmail.com>
2018-05-26 16:13:13 +0200

    all

---
+++ projects/mem_test/target/debug/deps/mem_test-fb2dc88e245e52af.d
@@ -0,0 +1,5 @@
+/usr/home/haetze/Documents/Code/DemoRust/projects/mem_test/target/debug/deps/mem_test-fb2dc88e245e52af: src/main.rs
+
+/usr/home/haetze/Documents/Code/DemoRust/projects/mem_test/target/debug/deps/mem_test-fb2dc88e245e52af.d: src/main.rs
+
+src/main.rs:
---
+++ projects/mem_test/target/debug/mem_test.d
@@ -0,0 +1,1 @@
+/usr/home/haetze/Documents/Code/DemoRust/projects/mem_test/target/debug/mem_test: /usr/home/haetze/Documents/Code/DemoRust/projects/mem_test/src/main.rs
