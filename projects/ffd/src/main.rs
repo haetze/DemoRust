884a7d2995ce0d45215e42959f1fdf080d6dbd8d
Richard Stewing <richy.sting@gmail.com>
2018-06-09 11:59:38 +0200

    more abstract

--- projects/ffd/src/main.rs
+++ projects/ffd/src/main.rs
@@ -1,3 +1,7 @@
+use std::cmp::Ordering::*;
+use std::cmp::PartialOrd;
+use std::ops::Add;
+
 fn main() {
     let objects: Vec<u32> = vec![3, 1, 4,
                                  3, 1,
@@ -7,23 +11,39 @@ fn main() {
 
     let partitions = ffd(objects, 10);
     println!("{:?}", partitions);
+
+    let objects: Vec<f32> = vec![0.3, 0.1, 0.4,
+                                 0.3, 0.1,
+                                 0.1, 0.4, 0.2,
+                                 0.3, 0.1, 0.4, 0.2];
+                                     
+
+    let partitions = ffd(objects, 1.0);
+    println!("{:?}", partitions);
+
     
 }
 
-fn ffd(mut objects: Vec<u32>, limit_capacity: u32) -> Vec<Vec<u32>> {
-    let mut partitions : Vec<Vec<u32>> = Vec::new();
-    objects.sort_by(|a, b| b.cmp(a));
+fn ffd<A: PartialOrd + Add<Output = A> + Clone + Copy>
+    (mut objects: Vec<A>, limit_capacity: A) -> Vec<Vec<A>> {
+        
+    let mut partitions : Vec<Vec<A>> = Vec::new();
+    objects.sort_by(|a, b| b.partial_cmp(a).unwrap_or(Equal));
 
     for i in objects {
         let mut added = false;
         for partition in &mut partitions {
-            let sum : u32 = partition.iter().sum();
+            let first = partition.iter().clone().nth(0).unwrap().clone();
+            let sum : A = partition.iter().clone().skip(1).fold(first, |acc, x| acc + *x);
+            
             if sum + i <= limit_capacity {
                 partition.push(i);
                 added = true;
                 break;
             }
+            
         }
+        
         if !added {
             let mut p = Vec::new();
             p.push(i);
