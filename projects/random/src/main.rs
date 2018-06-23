7ea1665c5bad7f47ff7dd3a367eb1054798b4715
Richard Stewing <richy.sting@gmail.com>
2018-06-23 15:12:08 +0200

    fib with fn gen

--- projects/random/src/main.rs
+++ projects/random/src/main.rs
@@ -105,6 +105,52 @@ impl<A: Iterator<Item=i32> + IsInfiniteSequence,
      B: Iterator<Item=i32> + IsInfiniteSequence> IsInfiniteSequence for Shuffler<A, B> {}
 
 
+
+struct GeneratorWithFn{
+    f: Box<Fn(&mut Vec<i32>) -> i32>,
+    v: Vec<i32>,
+}
+
+impl GeneratorWithFn{
+    fn new(f: Box<Fn(&mut Vec<i32>) -> i32>, v: Vec<i32>) -> Self{
+        GeneratorWithFn {
+            f: f,
+            v: v,
+        }
+    }
+    
+    fn new_fib(m: i32, v: Vec<i32>) -> Self {
+         GeneratorWithFn::new(Box::new(move |v: &mut Vec<i32>| {
+             let x = v.pop().unwrap();
+             let y = v.pop().unwrap();
+             let z = (x + y) % m;
+             v.push(y);
+             v.push(x);
+             v.push(z);
+             return z;
+         }),v)
+    }
+
+    // fn new_linear_con(m: i32, a: i32, c: i32, seed: i32) -> Self {
+    // }
+}
+
+impl Iterator for GeneratorWithFn {
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        use std::mem::swap;
+        let mut v = Vec::new();
+        swap(&mut v, &mut self.v);
+        let z = (self.f)(&mut v);
+        swap(&mut v, &mut self.v);
+        Some(z)
+            
+    }
+}
+
+impl IsInfiniteSequence for GeneratorWithFn{}
+    
+
 fn print_n_for_iter<A: Iterator<Item=i32> + IsInfiniteSequence>(mut iter: A, n: i32, name: String){
     println!("========={}=========", name);
     for i in 0..n {
@@ -124,4 +170,8 @@ fn main() {
     let rnd_gen_2 = LinearCongruentialMethod::new(1243, 284, 156, 73);
     let random_gen_3 = Shuffler::new(rnd_gen_1, rnd_gen_2, 321);
     print_n_for_iter(random_gen_3, 100, "Shuffler".to_string());
+
+    let random_gen_4 = GeneratorWithFn::new_fib(100, vec![7,8]);
+    print_n_for_iter(random_gen_4, 100, "FibGenFn".to_string());
+    
 }
