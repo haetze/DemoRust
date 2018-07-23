762f11466705ff9925a46d35044749d1a87f8216
Richard Stewing <richy.sting@gmail.com>
2018-07-23 22:06:32 +0200

    more examples

--- projects/lifetime_demo/src/main.rs
+++ projects/lifetime_demo/src/main.rs
@@ -1,5 +1,24 @@
 
-fn test<'a, A,B>(x: &'a A, y: &'a B, b: bool) -> Result<&'a A, &'a B> {
+// Works
+fn test_1<'a, A,B>(x: &'a A, y: &'a B, b: bool) -> Result<&'a A, &'a B> {
+    if b {
+        Ok(x)
+    }else {
+        Err(y)
+    }
+}
+
+// Doesn't work, needs lifetime explicitly, like above
+// fn test_2<A,B>(x: &A, y: &B, b: bool) -> Result<&A, &B> {
+//     if b {
+//         Ok(x)
+//     }else {
+//         Err(y)
+//     }
+// }
+
+//Also works with different lifetime
+fn test_3<'a, 'b, A,B>(x: &'a A, y: &'b B, b: bool) -> Result<&'a A, &'b B> {
     if b {
         Ok(x)
     }else {
@@ -9,9 +28,16 @@ fn test<'a, A,B>(x: &'a A, y: &'a B, b: bool) -> Result<&'a A, &'a B> {
 
 
 fn main() {
-    let result = test(&1,&2, true);
+    let result = test_1(&1,&2, true);
     println!("{:?}", result);
     
-    let result = test(&1,&2, false);
+    let result = test_1(&1,&2, false);
+    println!("{:?}", result);
+
+
+    let result = test_3(&1,&2, true);
+    println!("{:?}", result);
+    
+    let result = test_3(&1,&2, false);
     println!("{:?}", result);
 }
