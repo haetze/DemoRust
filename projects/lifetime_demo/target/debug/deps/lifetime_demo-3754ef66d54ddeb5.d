73adef8d852620b351e5d56c50d5eab1f1937c55
Richard Stewing <richy.sting@gmail.com>
2018-07-23 22:02:27 +0200

    lifetime demo

---
+++ projects/lifetime_demo/target/debug/deps/lifetime_demo-3754ef66d54ddeb5.d
@@ -0,0 +1,5 @@
+/usr/home/haetze/Documents/Code/DemoRust/projects/lifetime_demo/target/debug/deps/lifetime_demo-3754ef66d54ddeb5: src/main.rs
+
+/usr/home/haetze/Documents/Code/DemoRust/projects/lifetime_demo/target/debug/deps/lifetime_demo-3754ef66d54ddeb5.d: src/main.rs
+
+src/main.rs:
---
+++ projects/lifetime_demo/target/debug/lifetime_demo.d
@@ -0,0 +1,1 @@
+/usr/home/haetze/Documents/Code/DemoRust/projects/lifetime_demo/target/debug/lifetime_demo: /usr/home/haetze/Documents/Code/DemoRust/projects/lifetime_demo/src/main.rs
