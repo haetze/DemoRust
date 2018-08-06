c23ac2f754520c6008ba9b20b20b35892e00b153
Richard Stewing <richy.sting@gmail.com>
2018-08-06 12:49:50 +0200

    shuffle of slice after R. A. Fisher and F. Yates

--- projects/shuffle/src/main.rs
+++ projects/shuffle/src/main.rs
@@ -8,11 +8,11 @@ trait Shuffle {
 
 impl<A> Shuffle for [A] {
     fn shuffle(&mut self){
-        use std::mem::swap;
-        let t: f64 = self.len().into();
+        
         for j in (0..self.len()).rev() {
-            let U = rand::random::<f64>() % 1.0;
-            let k = (j.into() * U).floor() + 1;
+            let u = rand::random::<f32>() % 1.0;
+            let k = (j as f32 * u).floor() + 1.0;
+            self.swap(j, k as usize);
         }
     }
 }
