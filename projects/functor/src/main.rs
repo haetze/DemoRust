973c234cdd5f307cd0e29139ea5550b968133737
Richard Stewing <richy.sting@gmail.com>
2018-08-08 21:25:13 +0200

    Monad

--- projects/functor/src/main.rs
+++ projects/functor/src/main.rs
@@ -1,9 +1,15 @@
-
 trait Functor<T, U> {
     type Out;
     fn fmap<F>(self, f: F) -> Self::Out
         where F: Fn(T) -> U;
     
+}
+
+trait Monad<T, U> : Functor<T, U> {
+    type MonadOut;
+    fn eta(t: T) -> Self;
+    fn bind<F>(self, f: F) -> Self::MonadOut
+        where F: Fn(T) -> Self::MonadOut;
 }
 
 impl<A, B> Functor<A,B> for Option<A>{
@@ -17,6 +23,21 @@ impl<A, B> Functor<A,B> for Option<A>{
     }
 }
 
+impl<A, B> Monad<A, B> for Option<A> {
+    type MonadOut = Option<B>;
+    fn eta(a: A) -> Option<A> {
+        Some(a)
+    }
+    fn bind<F>(self, f: F) -> Self::MonadOut
+        where F: Fn(A) -> Self::MonadOut {
+        match self {
+            None => None,
+            Some(a) => f(a),
+        }
+    }
+}
+
+ 
 impl<A, B> Functor<A, B> for Vec<A> {
     type Out = Vec<B>;
     fn fmap<F>(self, f: F) -> Self::Out
