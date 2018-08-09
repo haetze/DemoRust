94bb52f4aff52cc10a4aaf81e57eac3a43641b96
Richard Stewing <richy.sting@gmail.com>
2018-08-09 23:07:19 +0200

    clean up

--- projects/async_read_keyboard/src/stdin_stream.rs
+++ projects/async_read_keyboard/src/stdin_stream.rs
@@ -6,10 +6,7 @@ use self::ncurses::*;
 use futures::{Future, Sink, Stream};
 use futures::sync::mpsc::channel;
 
-
-
 use std::thread;
-
 
 pub fn stdin() -> impl Stream<Item = char, Error = ()> {
     let (mut tx, rx) = channel(1);
