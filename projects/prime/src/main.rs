34d177ed6abd008b26e65b8304e70975f303ba0c
Richard Stewing <richy.sting@gmail.com>
2018-08-11 17:18:37 +0200

    primes till

--- projects/prime/src/main.rs
+++ projects/prime/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    for i in 1..100 {
-        println!("{} is prime? {}", i, is_prime(i));
-        println!("{} is prime? {}", i, is_prime_2(i));
-    }
+    // for i in 1..100 {
+    //     println!("{} is prime? {}", i, is_prime(i));
+    //     println!("{} is prime? {}", i, is_prime_2(i));
+    // }
+    
 }
 
 fn is_prime(u: u32) -> bool {
@@ -23,4 +24,14 @@ fn is_prime_2(u: u32) -> bool {
         0...1 => false,
         _     => (2..u).all(|x| u % x != 0),
     }
+}
+
+fn primes_til(u: u32) -> Vec<u32> {
+    let mut v = vec![2];
+    for i in 3..u {
+        if v.iter().all(|x| i % x != 0) {
+            v.push(i);
+        }
+    }
+    v
 }
