ceafe321244c5be2a46576be2f76bcce781772ba
Richard Stewing <richy.sting@gmail.com>
2018-08-11 17:13:31 +0200

    with iter

--- projects/prime/src/main.rs
+++ projects/prime/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     for i in 1..100 {
         println!("{} is prime? {}", i, is_prime(i));
+        println!("{} is prime? {}", i, is_prime_2(i));
     }
 }
 
@@ -15,4 +16,11 @@ fn is_prime(u: u32) -> bool {
         }
     }
     return true;
+}
+
+fn is_prime_2(u: u32) -> bool {
+    match u {
+        0...1 => false,
+        _     => (2..u).all(|x| u % x != 0),
+    }
 }
