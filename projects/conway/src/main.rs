bee7ac87da26fc8e1cf06f617f76cca2f055000f
Richard Stewing <richy.sting@gmail.com>
2018-08-26 23:59:36 +0200

    reading from file

--- projects/conway/src/main.rs
+++ projects/conway/src/main.rs
@@ -3,7 +3,11 @@ extern crate rayon;
 mod board;
 use board::Board;
 
+mod read;
+use read::read_from_file;
+
 use std::{thread, time};
+use std::env;
 
 fn main() {
     let v_1 = vec![false, false, false, false, false];
@@ -12,12 +16,17 @@ fn main() {
     let v_4 = vec![false, false, true, false, false];
     let v_5 = vec![false, false, false, false, false];
 
-    let f = vec![v_1, v_2, v_3, v_4, v_5];
+    let f = match env::args().skip(1).next() {
+        None => vec![v_1, v_2, v_3, v_4, v_5],
+        Some(s) => {
+            read_from_file(s)
+        },
+    };
     let mut board = Board::new(f);
 
     loop {
         println!("{}", board.show());
-        let ten_millis = time::Duration::from_millis(1000);
+        let ten_millis = time::Duration::from_millis(100);
         thread::sleep(ten_millis);
         board = board.step();
         
