225f365e28171d6023d0eaeef8e6fbc1df7a3231
Richard Stewing <richy.sting@gmail.com>
2019-03-23 14:51:54 +0100

    rucksack

--- projects/dyn_prog/src/main.rs
+++ projects/dyn_prog/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn fib(n: u64) -> u64 {
     if n == 0 {
         0
@@ -21,6 +23,95 @@ fn fib_dyn(n:u64) -> u64 {
     fib_seq[0]
 }
 
+fn value(objects: &Vec<(usize, usize)>, k: usize) -> usize {
+    value_rec(objects, 0, k)
+}
+
+fn value_rec(objects: &Vec<(usize, usize)>, i: usize, k: usize) -> usize {
+    let n = objects.len() - 1;
+    let (weight, value) = objects[i];
+    if i == n {
+        if weight > k {
+            return 0;
+        } else { 
+            return value;
+        }
+    } else if i < n {
+        let a = value_rec(objects, i+1, k);
+        if weight > k {
+            return a;
+        } else {
+            let b = value_rec(objects, i+1, k-weight) + value;
+            if a < b {
+                return b;
+            } else {
+                return a;
+            }
+        }
+    }
+    panic!("No Solution");
+}
+
+fn value_dyn(objects: &Vec<(usize, usize)>, k: usize) -> usize {
+    let mut map: HashMap<(usize, usize), usize> = HashMap::new();
+    value_dyn_calc(objects, 0, k, objects.len()-1, &mut map)
+}
+
+fn value_dyn_calc(objects: &Vec<(usize, usize)>,
+                  i: usize,
+                  k: usize,
+                  n: usize,
+                  map: &mut HashMap<(usize, usize), usize>) -> usize {
+    let key = (i,k);
+    match map.get(&key) {
+        Some(result) => result.clone(),
+        None => {
+            let (weight, value) = objects[i];
+            if i == n {
+                if weight > k {
+                    map.insert(key, 0);
+                    return 0;
+                } else {
+                    map.insert(key, value);
+                    return value;
+                }
+            } else if i < n {
+                let a = match map.get(&(i+1, k)) {
+                    None => value_dyn_calc(objects,
+                                           i+1,
+                                           k,
+                                           n,
+                                           map),
+                    Some(n) => n.clone(),
+                };
+                if weight > k {
+                    map.insert(key, a);
+                    return a;
+                } else {
+                    let b = match map.get(&(i+1, k- weight)) {
+                        None => value_dyn_calc(objects,
+                                               i+1,
+                                               k-weight,
+                                               n,
+                                               map) + value,
+                        Some(n) => n.clone() + value,
+                    };
+                    if a > b {
+                        map.insert(key, a);
+                        return a;
+                    } else {
+                        map.insert(key, b);
+                        return b;
+                    }
+                }
+                
+            }
+            panic!("No Solution");                    
+        },
+    }
+}
+
+
 fn main() {
     for x in 0..40 { 
         let fib_x = fib_dyn(x);
@@ -31,5 +122,19 @@ fn main() {
 
         println!("--------------------");
     }
+    let dinge = vec![(2,2)
+                     ,(2,3)
+                     ,(6,5)
+                     ,(5,4)
+                     ,(4,6)];
+    let kapazitaet = 10;
+    println!("Rucksack-----------------");
+    println!("Dinge: {:?}", dinge);
+    let sol = value(&dinge, kapazitaet);
+    println!("Loesung: {}", sol);
+    println!("Dinge: {:?}", dinge);
+    let sol = value_dyn(&dinge, kapazitaet);
+    println!("Loesung: {}", sol);
+    
 
 }
