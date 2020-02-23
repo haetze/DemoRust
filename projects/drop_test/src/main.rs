a6132030cdefdf718c5dae7e5bdd881e6587492b
Richard Stewing <richard.stewing@udo.edu>
2020-02-23 11:30:58 +0100

    drops with state

--- projects/drop_test/src/main.rs
+++ projects/drop_test/src/main.rs
@@ -1,6 +1,15 @@
 use std::sync::Arc;
+use std::sync::Mutex;
+use std::collections::HashSet;
 
-#[derive(Debug)]
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref SET : Mutex<HashSet<u32>> = Mutex::new(HashSet::new());
+}
+    
+#[derive(Debug, std::hash::Hash, std::cmp::PartialEq, std::cmp::Eq)]
 struct MyRecStruct {
     val : u32,
     inner : Option<Arc<MyRecStruct>>,
@@ -9,28 +18,32 @@ struct MyRecStruct {
 impl Drop for MyRecStruct {
     fn drop(&mut self) {
         println!("{}", self.val);
+        SET.lock().unwrap().insert(self.val);
     }
 }
 
 fn main() {
-    let a = MyRecStruct {
-        val : 0 ,
-        inner : None,
-    };
+    let mut test_set = HashSet::new();
 
-    let c = Arc::new(a);
-    let d = c.clone();
-    let _b = MyRecStruct {
-        val : 1,
-        inner : Some(c.clone()),
-    };
+    {
+        let a = MyRecStruct {
+            val : 0 ,
+            inner : None,
+        };
 
-    let _e = MyRecStruct {
-        val : 2,
-        inner : Some(d),
-    };
-
-    
-    println!("{:?}", c);
-    
+        let c = Arc::new(a);
+        let d = c.clone();
+        let _b = MyRecStruct {
+            val : 1,
+            inner : Some(c),
+        };
+     
+        let e = MyRecStruct {
+            val : 2,
+            inner : Some(d),
+        };
+        test_set.insert(e);
+        test_set;
+    }
+    println!("{:?}", SET.lock().unwrap());
 }
