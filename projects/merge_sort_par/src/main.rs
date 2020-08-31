2fcc4386e8dc8d2f35bf9a5a826eb92b41170bb5
Richard Stewing <richard.stewing@udo.edu>
2020-08-31 21:50:15 +0200

    x

--- projects/merge_sort_par/src/main.rs
+++ projects/merge_sort_par/src/main.rs
@@ -1,37 +1,34 @@
-use std::env;
 use async_std::task;
 use async_std::prelude::FutureExt;
 use std::time::SystemTime;
 use futures::future::{BoxFuture, FutureExt as FExt};
-use std::fs::OpenOptions;
-use std::io::prelude::*;
 
-const LIMIT : usize = 64;
+const LIMIT : usize = 256;
 
-fn split(mut arr : Vec<u32>) -> (Vec<u32>, Vec<u32>) {
-    let b = arr.split_off(arr.len()/2);
-    return (arr, b);
+fn split(arr : &[u32]) -> (&[u32], &[u32]) {
+    let l = arr.len()/2;
+    return (&arr[..l], &arr[l..]);
 }
 
-fn merge(a : Vec<u32>, b : Vec<u32>) -> Vec<u32> {
-    let n = a.len();
+fn merge(a : &[u32], b : &[u32]) -> Vec<u32> {
+    let l = a.len();
     let m = b.len();
-    let mut v = vec![0;n+m];
+    let mut v = vec![0;l+m];
 
     let mut i = 0;
     let mut j = 0;
-    for l in &mut v {
-	if i >= n {
-	    *l = b[j];
+    for e in &mut v {
+	if i >= l {
+	    *e = b[j];
 	    j += 1;
 	} else if j >= m {
-	    *l = a[i];
+	    *e = a[i];
 	    i += 1;
 	} else if a[i] > b[j] {
-	    *l = b[j];
+	    *e = b[j];
 	    j += 1;
 	} else {
-	    *l = a[i];
+	    *e = a[i];
 	    i += 1;
 	}
     }
@@ -45,13 +42,13 @@ fn sort(mut arr : Vec<u32>) -> BoxFuture<'static, Vec<u32>> {
 	    arr.sort();
 	    return arr;
 	}
-	let (a,b) = split(arr);
-	let a = sort(a);
-	let b = sort(b);
+	let (a,b) = split(&arr);
+	let a = sort(a.to_vec());
+	let b = sort(b.to_vec());
 	let a = task::spawn(a);
 	let b = task::spawn(b);
 	let (a,b) = a.join(b).await;
-	merge(a,b)
+	merge(&a,&b)
     }.boxed()
 }
 
@@ -60,60 +57,41 @@ fn sort_seq(mut arr : Vec<u32>) -> Vec<u32> {
 	arr.sort();
 	return arr;
     }
-    let (a, b) = split(arr);
-    let a = sort_seq(a);
-    let b = sort_seq(b);
-    let arr_s = merge(a,b);
+    let (a, b) = split(&arr);
+    let a = sort_seq(a.to_vec());
+    let b = sort_seq(b.to_vec());
+    let arr_s = merge(&a,&b);
     return arr_s;
 }
 
+
+
+
 #[async_std::main]
 async fn main() -> () {
-    let args : Vec<String> = env::args().collect();
-    let m : u32 = args[1].parse().unwrap_or(1024);
-    std::fs::remove_file("sort.dat").unwrap();
-    let mut file = OpenOptions::new()
-        .write(true)
-        .create(true)
-	.append(true)
-        .open("sort.dat")
-	.unwrap();
+    let n : u32 = (2 as u32).pow(25);
+    
+    //Merge Sort (parallel)
+    let v : Vec<u32> = (1..n).rev().collect();
+    let start =  SystemTime::now();
+    let _u_sorted = task::spawn(sort(v)).await;
+    let merge_p_time = start.elapsed().unwrap().as_nanos();
+    println!("{:?}", merge_p_time);
 
-    file.write_all(b"Length,merge parallel, merge seq, built in\n").unwrap();
-    let two : u32 = 2;
-    for n in 1..m+1 {
-	let n = two.pow(n);
-	let mut v : Vec<u32> = Vec::with_capacity(n as usize);
-	{ 
-	    let mut m = n;
-	    for _ in 0..n {
-		v.push(m);
-		m = m - 1;
-	    }
-	}
+    //Merge Sort (seq)
+    let v : Vec<u32> = (1..n).rev().collect();
+    let start =  SystemTime::now();
+    let _u_sorted = sort_seq(v);
+    let merge_s_time = start.elapsed().unwrap().as_nanos();
+    println!("{:?}", merge_s_time);
 
-	//Merge Sort (parallel)
-	let u = v.clone();
-	let start =  SystemTime::now();
-	let u_sorted = task::spawn(sort(u)).await;
-	let merge_p_time = start.elapsed().unwrap().as_nanos();
-	//println!("{:?}", u_sorted);
+    //Merge Sort (seq)
+    let mut v : Vec<u32> = (1..n).rev().collect();
+    let start =  SystemTime::now();
+    v.sort();
+    let sort_b_time = start.elapsed().unwrap().as_nanos();
+    println!("{:?}", sort_b_time);
 
-	//Merge Sort (seq)
-	let u = v.clone();
-	let start =  SystemTime::now();
-	let _u_sorted = sort_seq(u);
-	let merge_s_time = start.elapsed().unwrap().as_nanos();
-	
-	//Merge Sort (seq)
-	let mut u = v.clone();
-	let start =  SystemTime::now();
-	u.sort();
-	let sort_b_time = start.elapsed().unwrap().as_nanos();
+    return 
 
-	let s = format!("{},{},{},{}\n", n, merge_p_time, merge_s_time, sort_b_time);
-	file.write_all(s.as_bytes()).unwrap();
-	println!("{}: ✅", n);
-
-    }
 }
